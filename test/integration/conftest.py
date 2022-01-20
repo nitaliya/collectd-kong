ae9918efb42a77a5dd1d7b938517891a36d720a4
nitaliya <nitaliya@splunk.com>
2022-01-20 17:25:44 +0530

    test

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -39,10 +39,9 @@ def kong_image_and_version(request):
     version, shared_dict_line_number = request.param
     dockerfile = BytesIO(bytes(dedent(r'''
         FROM kong:{version}
-        RUN yum install -y epel-release python3
-        RUN yum install -y python3-pip postgresql
-        RUN ln -sf python3 /usr/bin/python
-        RUN pip3 install cqlsh
+        RUN yum install -y epel-release
+        RUN yum install -y python-pip postgresql
+        RUN python -m pip install cqlsh
         WORKDIR /usr/local/share/lua/5.1/kong
         RUN sed -i '{line_num}ilua_shared_dict kong_signalfx_aggregation 10m;' templates/nginx_kong.lua
         RUN sed -i '{line_num}ilua_shared_dict kong_signalfx_locks 100k;' templates/nginx_kong.lua
